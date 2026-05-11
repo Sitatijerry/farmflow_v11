c30fa18c622b333918640ad5022f0fbd5ad74a9f
Jerry Sitati <jerryssitati@gmail.com>
2026-05-11 15:09:51 +0300

    scaffolded front&backend

--- client/src/TRPCProvider.tsx
+++ client/src/TRPCProvider.tsx
@@ -9,7 +9,7 @@ export function TRPCProvider({ children }: { children: React.ReactNode } ) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: 'http://localhost:3001/trpc',
+          url: `${window.location.origin}/trpc`,
         } ),
       ],
     })
